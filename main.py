a6b57334fe7b288b86d7e1d5f296472ed95dfb09
Bedanta Dey <deybedanta17@proton.me>
2023-02-18 21:16:50 +0530

    added new functionality

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-import string
 import random
+import string
+import datetime
 
-# define a function to generate a random password
+# Get the current date and time
+now = datetime.datetime.now()
+
+# Define a function to generate a random password
 def generate_password(length):
-    letters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(letters) for i in range(length))
+    # Define the set of characters to choose from
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Generate a random password using the set of characters
+    password = ''.join(random.choice(characters) for i in range(length))
+
+    # Return the generated password
     return password
 
-# take input for the length of the password
-password_length = int(input("Enter the length of the password: "))
+# Generate a random password with a length of 12 characters
+password = generate_password(12)
 
-# generate a random password
-password = generate_password(password_length)
+# Create a file to save the password and date/time information
+filename = 'passwords.txt'
 
-# save the password to a text file
-with open("passwords.txt", "a") as file:
-    file.write(password + "\n")
-    print("Password saved to passwords.txt")
+# Write the password and date/time information to the file
+with open(filename, 'a') as file:
+    file.write('Password: ' + password + '\n')
+    file.write('Generated on: ' + now.strftime('%Y-%m-%d %H:%M:%S') + '\n\n')
+
+# Print a message to indicate that the password has been saved in the file
+print('Your password has been successfully generated. \nRefer to "passwords.txt" to view it.')
